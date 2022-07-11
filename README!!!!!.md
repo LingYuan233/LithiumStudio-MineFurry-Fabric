86cfa7d84cd507b847165f413cb9a3c41b476d69
Ling_Yuan <336128028@qq.com>
2022-07-11 18:26:04 +0800

    0.0-22.7.1-40.1.58

---
+++ README!!!!!.md
@@ -0,0 +1,17 @@
+## 我的世界MineFurryMod
+
+--- 
+
+> ！注意 ！
+>> * 该Mod为 MineFurry 的Fabric移植版
+>> * MineFurry(Forge)与MineFurry(Fabric)均为磷酸铁狸工作室(LithiumStudio)制作出版并发布
+>> * MineFurry(Forge)与MineFurry(Fabric)均使用GNU GPL 2.0开源协议
+>> * /src/main/resource/assets/mf/textures/painting/下所有文件均已授权，未经设定本人允许<u>禁止使用</u>，详情请见该文件夹下的readme文件
+---
+#### 介绍
+你是否曾经幻想过：如果自己变成了一只或可爱，或帅气的兽兽，那该有~~多涩~~多好啊~
+既然现实中做不到这些，那么这款模组或将会帮你满足这些愿望
+
+##### 这个模组里有什么
+这个模组目前包含了制作兽装的前置条件——DTD，与一张音乐唱片
+未来一定会变得更加有趣，更加真实，更能满足各位的需求
--- src/main/java/lithiumstudio/mf/painting/Paintings.java
+++ src/main/java/lithiumstudio/mf/painting/Paintings.java
@@ -6,22 +6,22 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.world.IdCountsState;
 
 public class Paintings {
-    private static int px(int i){
+    private static int chunk(int i){
         return i*16;
     }
     public static void register() {
-        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0000"), new PaintingMotive(px(1),px(1)));
-        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0001"), new PaintingMotive(px(2),px(2)));
-        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0002"), new PaintingMotive(px(3),px(2)));
-        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0003"), new PaintingMotive(px(3),px(4)));
-        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0004"), new PaintingMotive(px(4),px(5)));
-        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0005"), new PaintingMotive(px(1),px(2)));
-        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0006"), new PaintingMotive(px(16),px(9)));
-        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0007"), new PaintingMotive(px(3),px(4)));
-        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0008"), new PaintingMotive(px(1),px(2)));
-        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0009"), new PaintingMotive(px(2),px(2)));
-        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0010"), new PaintingMotive(px(5),px(4)));
-        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0011"), new PaintingMotive(px(5),px(4)));
-        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0012"), new PaintingMotive(px(5),px(4)));
+        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0000"), new PaintingMotive(chunk(1),chunk(1)));
+        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0001"), new PaintingMotive(chunk(2),chunk(2)));
+        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0002"), new PaintingMotive(chunk(3),chunk(2)));
+        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0003"), new PaintingMotive(chunk(3),chunk(4)));
+        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0004"), new PaintingMotive(chunk(4),chunk(5)));
+        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0005"), new PaintingMotive(chunk(1),chunk(2)));
+        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0006"), new PaintingMotive(chunk(16),chunk(9)));
+        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0007"), new PaintingMotive(chunk(3),chunk(4)));
+        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0008"), new PaintingMotive(chunk(1),chunk(2)));
+        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0009"), new PaintingMotive(chunk(2),chunk(2)));
+        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0010"), new PaintingMotive(chunk(5),chunk(4)));
+        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0011"), new PaintingMotive(chunk(5),chunk(4)));
+        Registry.register(Registry.PAINTING_MOTIVE, new Identifier("mf","img_0012"), new PaintingMotive(chunk(5),chunk(4)));
     }
 }
